2b7715853e3a3b32e08db3cf92ae522a878dba0e
Kingcedru <byishimocedrick@gmail.com>
2023-10-17 18:26:11 +0200

    first set up

---
+++ src/components/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Registration() {
+    return(
+        <div>
+            <form className='grid gap-5'>
+                <label>Names<input className='border-2 ' type='text'/></label>
+                <label>Age<input className='border-2 ' type='text'/></label>
+            </form>
+        </div>
+) 
+}
