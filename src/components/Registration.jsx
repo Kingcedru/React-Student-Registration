990c42b751892cbf937b4d21495e8d3fab36fd6e
Kingcedru <byishimocedrick@gmail.com>
2023-10-18 23:13:29 +0200

    styling

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,5 +1,4 @@
-import React, {useState } from 'react'
-import Students from './Students'
+import  {useState } from 'react'
 export default function Registration() {
     const [name,nextName] = useState('')
     const [age,nextAge] = useState('')
@@ -10,30 +9,38 @@ export default function Registration() {
 const register = (e)=>{
     e.preventDefault();
     nextData([...data,{name,age,country,description}])
+    nextName('')
+    nextAge('')
+    nextCountry('')
+    nextDescription('')
+}
+const deleteStudent = (id)=>{
+    return nextData(data.filter((item,index) => index !== id))
 }
     return(
         <div className='flex justify-center w-full'>
         <div className='grid gap-20'>
-            <form className='grid gap-5 bg-indigo-600 px-20 py-10 rounded-xl  h-96' onSubmit={register}>
-                <label>Names<input className='border' type='text' required onChange={(e)=>nextName(e.target.value)}/></label>
-                <label>Age<input className='border' type='text' required onChange={(e)=>nextAge(e.target.value)}/></label>
-                <select className='border' value={country} onChange={(e)=> nextCountry(e.target.value)}>
-                    <option value=""></option>
+            <form className='grid gap-5 bg-indigo-600 px-20 py-10 rounded-xl ' onSubmit={register}>
+                <label>Names</label><input className='border rounded-full py-2 px-10' type='text' value={name} placeholder='Enter Names' required onChange={(e)=>nextName(e.target.value)}/>
+                <label>Age</label><input className='border rounded-full py-2 px-10' type='text' value={age} placeholder='Enter Age' required onChange={(e)=>nextAge(e.target.value)}/>
+                <select className='border py-2' value={country} onChange={(e)=> nextCountry(e.target.value)}>
+                    <option value="">Country</option>
                     <option value="Rwanda">Rwanda</option>
                     <option value="Burundi">Burindi</option>
-                    <option value="Rwanda">Rwanda</option>
+                    <option value="Uganda">Uganda</option>
                 </select>
-                <textarea className='border' value={description} onChange={(e)=> nextDescription(e.target.value)}></textarea>
-                <input className='border' type='submit'/>
+                <textarea className='border' placeholder='Enter description' value={description} onChange={(e)=> nextDescription(e.target.value)}></textarea>
+                <input className='border rounded-full text-white font-bold hover:bg-red-500 bg-red-600 py-3 w-40' type='submit'/>
             </form> 
             <div>
-            <table className='table-auto border border-slate-500'>
-                <thead>
+            <table className=' border border-slate-500'>
+                <thead className=''>
                     <tr className=''>
-                    <th className='border border-slate-600'>Names</th>
-                    <th className='border border-slate-600'>Age</th>
-                    <th className='border border-slate-600'>Country</th>
-                    <th className='border border-slate-600'>Description</th>
+                    <th className='border border-slate-600 px-10'>Names</th>
+                    <th className='border border-slate-600 px-10'>Age</th>
+                    <th className='border border-slate-600 px-10'>Country</th>
+                    <th className='border border-slate-600 px-10'>Description</th>
+                    <th className='border border-slate-600 px-10'>Status</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,10 +48,17 @@ const register = (e)=>{
                 return(
                     
                     <tr key={index} className=''>
-                        <td className='border border-slate-700'>{item.name}</td>
-                        <td className='border border-slate-700'>{item.age}</td>
-                        <td className='border border-slate-700'>{item.country}</td>
-                        <td className='border border-slate-700'>{item.description}</td>
+                        <td className='border border-slate-700 px-10'>{item.name}</td>
+                        <td className='border border-slate-700 px-10'>{item.age}</td>
+                        <td className='border border-slate-700 px-10'>{item.country}</td>
+                        <td className='border border-slate-700 px-10'>{item.description}</td>
+                        <td className='border border-slate-700 px-10'>
+                            <select>
+                                <option value="">Pending</option>
+                                <option value="pending">Edit</option>
+                                <option value={data} onClick={()=>deleteStudent(index)}>Delete</option>
+                            </select>
+                        </td>
                     </tr>
                     
                 )
